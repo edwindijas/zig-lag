c835fd4ce8293d7d94c51f8ce345d6b83fdfd9ca
Edwin Chiwona <edwindijas@hotmail.com>
2026-05-21 22:15:25 +0200

    add storybook

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,7 +1,8 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import rootConfig from '../../eslint.config.js'
 
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  ...rootConfig,
-])
+export default defineConfig([...rootConfig, ...storybook.configs["flat/recommended"]])
