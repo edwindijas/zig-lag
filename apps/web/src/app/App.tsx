640325c9dc71e70afc79825bbbc893f196797d36
Edwin Chiwona <edwindijas@hotmail.com>
2026-05-21 15:45:33 +0200

    refractor: change reset css to be part of app

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -2,10 +2,12 @@ import type { ReactNode } from 'react';
 
 import { RouterProvider } from './router';
 import { StoreProvider } from './store/provider';
+import { GlobalResetCSS } from './styles/reset';
 
 export const App = (): ReactNode => {
   return (
     <StoreProvider>
+      <GlobalResetCSS />
       <RouterProvider />
     </StoreProvider>
   );
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,13 +4,11 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 
 import { appInitialiser } from './app/boot';
-import { GlobalResetCSS } from './app/styles/reset';
 
 appInitialiser();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalResetCSS />
     <App />
   </StrictMode>,
 );
